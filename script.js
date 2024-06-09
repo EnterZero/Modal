1116e1a61d638dd7d4d4fdae218d13db2dd66169
Christian Kalis <c1x.chaz1q@gmail.com>
2024-06-10 00:18:41 +0200

    v 1

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+'use strict';
 
+const open = document.querySelectorAll('.show-modal');
+const close = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+// open
+const openFunction = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < open.length; i++)
+    open[i].addEventListener('click', openFunction);
+
+// close
+const closeFunction = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+close.addEventListener('click', closeFunction);
+overlay.addEventListener('click', closeFunction);
+document.addEventListener('keydown', function (press) {
+    if (press.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeFunction();
+    }
+})
